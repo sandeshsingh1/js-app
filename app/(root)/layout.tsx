06cee7a84f00b5535fccf80ecd38dbb4886541ac
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-01-14 14:18:45 +0530

    Done group Routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
-const layout = () => {
+const layout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div>layout</div>
+    <div>
+       <p> Navbar</p>
+       {children}
+    </div>
   )
 }
-
 export default layout
